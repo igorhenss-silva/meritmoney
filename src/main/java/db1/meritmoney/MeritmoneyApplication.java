ac53a7c05a47aead02ed429d4a1a06f3f5311824
Igor Henrique Salviano da Silva <igor.silva@db1.com.br>
2019-06-13 14:49:40 -0300

    Projeto MeritMoney criado!

---
+++ src/main/java/db1/meritmoney/MeritmoneyApplication.java
@@ -0,0 +1,13 @@
+package db1.meritmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeritmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeritmoneyApplication.class, args);
+	}
+
+}
