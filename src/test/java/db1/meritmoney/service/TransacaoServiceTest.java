7d56d9826a421392192403694640e84905c4d767
igorhenss <igorhenss@gmail.com>
2019-06-28 13:07:51 -0300

    TransacaoServiceTest adicionado, vazio

--- src/test/java/db1/meritmoney/service/TransacaoServiceTest.java
+++ src/test/java/db1/meritmoney/service/TransacaoServiceTest.java
@@ -1,4 +1,22 @@
 package db1.meritmoney.service;
 
+import db1.meritmoney.domain.entity.Transacao;
+import db1.meritmoney.repository.TransacaoRepository;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TransacaoServiceTest {
+
+    @Autowired
+    TransacaoService transacaoService;
+
+    @Autowired
+    TransacaoRepository transacaoRepository;
+
+//    Transacao transacao = new Transacao();
+
 }
